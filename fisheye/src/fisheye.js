01db500dccb8f4594c491c9d88df76ecc75506b6
Wendyx <xiawenyu9802@163.com>
2020-09-11 10:40:03 +0800

    fisheye lens, bug with boundary grid

--- fisheye/src/fisheye.js
+++ fisheye/src/fisheye.js
@@ -8,7 +8,6 @@ const nodeColor = 0x1ABC9C;
 const selectColor = 0xE74C3C;
 const lineColorDark = 0x2E4053;
 const lineColorLight = 0xF0EDE4;
-const resolution = 1;
 //parameters
 const stageWidth_pix = 800, stageHeight_pix = 800;
 const tableWidth = nodeCnt, tableHeight = nodeCnt;
@@ -18,6 +17,7 @@ const linechartsTextureScale = 2;
 const maxScale = 1.5;
 const chartRatio=0.8;
 const nodeRadius =0.2;
+const EP = 0;
 //random data
 const data= {};
 for(let t=timeStart;t<=timeEnd;t++) {
@@ -100,40 +100,24 @@ function rgbStrToHex(s) {
 function createBackgroundTexture(h1,w1,h2,w2) {
     const width = w2-w1+1;
     const height=h2-h1+1;
-    const rgba = new Float32Array(width*height*4*resolution*resolution);
+    const rgba = new Float32Array(width*height*4);
     for(let i=0;i<height;i++) {
         for(let j=0;j<width;j++) {
+            if(i+h1<0||j+w1<0||i+h1>=tableHeight||j+w1>=tableWidth) {
+                continue;
+            }
             let value = getValue(timeEnd,i+h1,j+w1);
             let colorStr = d3.interpolateYlGn(value);
             let color = rgbStrToHex(colorStr);
-            for(let p=0;p<resolution;p++) {
-                for(let q=0;q<resolution;q++) {
-                    let idx = (i*resolution+p)*resolution*width+(j*resolution+q);
-                    rgba[idx*4] = (color>>16)/256;
-                    rgba[idx*4+1] = ((color>>8)&((1<<8)-1))/256;
-                    rgba[idx*4+2] = (color&((1<<8)-1))/256;
-                    rgba[idx*4+3] = 1;     
-                }
-            }
+            let idx = i*width+j;
+            rgba[idx*4] = (color>>16)/256;
+            rgba[idx*4+1] = ((color>>8)&((1<<8)-1))/256;
+            rgba[idx*4+2] = (color&((1<<8)-1))/256;
+            rgba[idx*4+3] = 1;
         }
-    }
-    const texture = PIXI.Texture.fromBuffer(rgba,width*resolution,height*resolution);
+    }   
+    const texture = PIXI.Texture.fromBuffer(rgba,width,height);
     return texture;
-    //const rgba = new Float32Array(width*height*4);
-    //for(let i=0;i<height;i++) {
-    //    for(let j=0;j<width;j++) {
-    //        let value = getValue(timeEnd,i+h1,j+w1);
-    //        let colorStr = d3.interpolateYlGn(value);
-    //        let color = rgbStrToHex(colorStr);
-    //        let idx = i*width+j;
-    //        rgba[idx*4] = (color>>16)/256;
-    //        rgba[idx*4+1] = ((color>>8)&((1<<8)-1))/256;
-    //        rgba[idx*4+2] = (color&((1<<8)-1))/256;
-    //        rgba[idx*4+3] = 1;
-    //    }
-    //}   
-    //const texture = PIXI.Texture.fromBuffer(rgba,width,height);
-    //return texture;
 }
 
 const backgroundTexture = createBackgroundTexture(0,0,tableHeight-1,tableWidth-1);
@@ -148,8 +132,9 @@ function createLineChartsSvg(gridH,gridW,h1,w1,h2,w2) {
     const chartWidth = gridW*chartRatio;
     const originW = (gridW-chartWidth)/2;
     const originH = (gridH-chartHeight)/2;
-    for(let i=h1;i<=h2;i++) {
-        for(let j=w1;j<=w2;j++) {
+    for(let i=Math.max(h1,0);i<=Math.min(h2,tableHeight-1);i++) {
+        for(let j=Math.max(w1,0);j<=Math.min(w2,tableWidth-1);j++) {
+            
             let translateW = (j-w1)*gridW;
             let translateH = (i-h1)*gridH;
             let xScale = d3.scaleLinear()
@@ -217,21 +202,21 @@ function svgToBase64(svg) {
 }
 //init app
 const container = new PIXI.Container();
+container.x = EP/2;
+container.y=EP/2;
 let canvas = document.getElementById("mycanvas");
-let app = new PIXI.Application({width:stageWidth_pix, height:stageHeight_pix, antialias:true, view:canvas});
+let app = new PIXI.Application({width:stageWidth_pix+EP, height:stageHeight_pix+EP, antialias:true, view:canvas});
 app.renderer.backgroundColor = backgroundColor;
 //document.body.appendChild(app.view);
 app.stage.interactive = true;
 app.stage.addChild(container);
 
 const backgroundSprite = new PIXI.Sprite(backgroundTexture);
-backgroundSprite.scale.x = stepWidth_pix/resolution;
-backgroundSprite.scale.y = stepHeight_pix/resolution;
-//backgroundSprite.scale.x = stepWidth_pix;
-//backgroundSprite.scale.y = stepHeight_pix;
+backgroundSprite.scale.x = stepWidth_pix;
+backgroundSprite.scale.y = stepHeight_pix;
 backgroundSprite.x=0;
 backgroundSprite.y=0;
-container.addChild(backgroundSprite);
+//container.addChild(backgroundSprite);
 
 var linechartsSprite = new PIXI.Sprite();
 linechartsSprite.scale.x=1/linechartsTextureScale;
@@ -264,7 +249,6 @@ const fragmentSrc = `
     varying vec2 vUvs;
 
     uniform sampler2D uSampler2;
-    uniform float time;
 
     void main() {
         gl_FragColor = texture2D(uSampler2, vUvs);
@@ -279,13 +263,16 @@ const shader = PIXI.Shader.from(vertexSrc, fragmentSrc, uniforms);
 var quad = new PIXI.Mesh(geometry, shader);
 quad.position.set(0,0);
 quad.interactive = true;
-quad.mousemove = function(e) {}
 canvas.addEventListener('mousemove',function(evt) {
     const rect = canvas.getBoundingClientRect();
-    let x_canvas = evt.clientX-rect.left;
-    let y_canvas = evt.clientY-rect.top;
-    let w = Math.floor(x_canvas/stepWidth_pix);
-    let h = Math.floor(y_canvas/stepHeight_pix);
+    let x_canvas = evt.clientX-rect.left-container.x;
+    let y_canvas = evt.clientY-rect.top-container.y;
+    //let w = Math.floor(x_canvas/stepWidth_pix);
+    //let h = Math.floor(y_canvas/stepHeight_pix);
+    let w = x_canvas/stepWidth_pix;
+    let h = y_canvas/stepHeight_pix;
+    //let w_num = Math.floor(w);
+    //let h_num = Math.floor(h);
     if(w<0||h<0||w>=tableWidth||h>=tableHeight) {
         return;
     }
@@ -298,33 +285,11 @@ canvas.addEventListener('mousemove',function(evt) {
         geometry = updateGeometry(h=h,w=w);
         quad.geometry = geometry;
     }
+    h = Math.floor(h);
+    w = Math.floor(w);
     updateLinechartsSprite(h,w);
 });
-container.addChild(quad);
-container.addChild(linechartsSprite);
-function updateLinechartsSprite(h,w) {
-    let frameWidth = (distortWidth-focalWidth)/2;
-    let frameHeight = (distortHeight-focalHeight)/2;
-    w = Math.max(frameWidth,Math.min(w,tableWidth-frameWidth-1));
-    h = Math.max(frameHeight,Math.min(h,tableHeight-frameHeight-1));
-    const focalOrigin = [(distortWidth-focalWidth*fisheyeScale)/2*stepWidth_pix,(distortHeight-focalWidth*fisheyeScale)/2*stepHeight_pix];
-    
-    let startW = w-Math.floor(distortWidth/2);
-    let startH = h-Math.floor(distortHeight/2);
-    console.log(`h = ${h}  w = ${w}`);
-    let bgTexture = createBackgroundTexture(startH,startW,startH+distortHeight-1,startW+distortWidth-1);
-    quad.shader.uniforms.uSampler2=bgTexture;
-    quad.x=startW*stepWidth_pix;
-    quad.y=startH*stepHeight_pix;
-    let innerW = w-Math.floor(focalWidth/2);
-    let innerH = h-Math.floor(focalHeight/2);
-    let linechartsTexture = createLineChartsTexture(stepHeight_pix*fisheyeScale,stepWidth_pix*fisheyeScale,innerH,innerW,innerH+focalHeight-1,innerW+focalWidth-1);
-    linechartsSprite.texture = linechartsTexture;
-    linechartsSprite.x = startW*stepWidth_pix+focalOrigin[0];
-    linechartsSprite.y = startH*stepHeight_pix+focalOrigin[1];
-    linechartsSprite.h=h;
-    linechartsSprite.w = w;
-}
+
 function updateGeometry(h,w) {
     const frameWidth = (distortWidth-focalWidth)/2;
     const frameHeight = (distortHeight-focalHeight)/2;
@@ -342,41 +307,138 @@ function updateGeometry(h,w) {
         focalOrigin[0]+focalWidth*fisheyeScale*stepWidth_pix,focalOrigin[1]+focalHeight*fisheyeScale*stepHeight_pix,
         focalOrigin[0],focalOrigin[1]+focalHeight*fisheyeScale*stepHeight_pix
     ];
-    const pos_list= [0,0,
-                distortWidth_pix,0,
-                distortWidth_pix,distortHeight_pix,
-                0,distortHeight_pix,
-                innerRec[0],innerRec[1],
-                innerRec[2],innerRec[3],
-                innerRec[4],innerRec[5],
-                innerRec[6],innerRec[7],
-                distortWidth_pix/2,0,
-                distortWidth_pix,distortHeight_pix/2,
-                distortWidth_pix/2,distortHeight_pix,
-                0,distortHeight_pix/2,
-                distortWidth_pix/2,focalOrigin[1],
-                innerRec[4],distortHeight_pix/2,
-                distortWidth_pix/2,innerRec[5],
-                focalOrigin[0],distortHeight_pix/2
-            ];
-    const pos_list_uv = [0, 0,
-                1, 0,
-                1, 1,
-                0, 1,
-                innerRec_uv[0],innerRec_uv[1],
-                innerRec_uv[2],innerRec_uv[3],
-                innerRec_uv[4],innerRec_uv[5],
-                innerRec_uv[6],innerRec_uv[7],
-                0.5,0,
-                1,0.5,
-                0.5,1,
-                0,0.5,
-                0.5,focalOrigin_uv[1],
-                innerRec_uv[4],0.5,
-                0.5,innerRec_uv[5],
-                focalOrigin_uv[0],0.5
-            ];
-    const index_list = [4,0,8,4,8,12,5,12,8,5,8,1,5,1,9,5,13,9,6,13,9,6,9,2,6,2,10,6,10,14,7,14,10,7,10,3,7,3,11,7,11,15,4,15,11,4,11,0,4,5,6,4,6,7];
+    //const pos_list= [0,0,
+    //            distortWidth_pix,0,
+    //            distortWidth_pix,distortHeight_pix,
+    //            0,distortHeight_pix,
+    //            innerRec[0],innerRec[1],
+    //            innerRec[2],innerRec[3],
+    //            innerRec[4],innerRec[5],
+    //            innerRec[6],innerRec[7],
+    //            distortWidth_pix/2,0,
+    //            distortWidth_pix,distortHeight_pix/2,
+    //            distortWidth_pix/2,distortHeight_pix,
+    //            0,distortHeight_pix/2,
+    //            distortWidth_pix/2,focalOrigin[1],
+    //            innerRec[4],distortHeight_pix/2,
+    //            distortWidth_pix/2,innerRec[5],
+    //            focalOrigin[0],distortHeight_pix/2
+    //        ];
+    //const pos_list_uv = [0, 0,
+    //            1, 0,
+    //            1, 1,
+    //            0, 1,
+    //            innerRec_uv[0],innerRec_uv[1],
+    //            innerRec_uv[2],innerRec_uv[3],
+    //            innerRec_uv[4],innerRec_uv[5],
+    //            innerRec_uv[6],innerRec_uv[7],
+    //            0.5,0,
+    //            1,0.5,
+    //            0.5,1,
+    //            0,0.5,
+    //            0.5,focalOrigin_uv[1],
+    //            innerRec_uv[4],0.5,
+    //            0.5,innerRec_uv[5],
+    //            focalOrigin_uv[0],0.5
+    //        ];
+    //const index_list = [4,0,8,4,8,12,5,12,8,5,8,1,5,1,9,5,13,9,6,13,9,6,9,2,6,2,10,6,10,14,7,14,10,7,10,3,7,3,11,7,11,15,4,15,11,4,11,0,4,5,6,4,6,7];
+    const pos_list = [],pos_list_uv=[],index_list = [];
+    for(let i=0;i<tableWidth;i++) {
+        pos_list.push(i*stepWidth_pix);
+        pos_list.push(0);
+        pos_list_uv.push(i/distortWidth);
+        pos_list_uv.push(0);
+    }
+    for(let i=0;i<tableHeight;i++) {
+        pos_list.push(distortWidth_pix);
+        pos_list.push(i*stepHeight_pix);
+        pos_list_uv.push(1);
+        pos_list_uv.push(i/distortHeight);
+    }
+    for(let i=tableWidth;i>0;i--) {
+        pos_list.push(i*stepWidth_pix);
+        pos_list.push(distortHeight_pix);
+        pos_list_uv.push(i/distortWidth);
+        pos_list_uv.push(1);
+    }
+    for(let i=tableHeight;i>0;i--) {
+        pos_list.push(0);
+        pos_list.push(i*stepHeight_pix);
+        pos_list_uv.push(0);
+        pos_list_uv.push(i/distortHeight);
+    }
+    for(let i=0;i<8;i++) {
+        pos_list.push(innerRec[i]);
+        pos_list_uv.push(innerRec_uv[i]);
+    }
+    let innerIndexStart = 2*(tableWidth+tableHeight);
+    //top
+    for(let i=0;i<Math.floor(tableWidth/2);i++) {
+        index_list.push(i);
+        index_list.push(i+1);
+        index_list.push(innerIndexStart);
+    }
+    index_list.push(Math.floor(tableWidth/2));
+    index_list.push(innerIndexStart);
+    index_list.push(innerIndexStart+1);
+    for(let i=Math.floor(tableWidth/2);i<tableWidth;i++) {
+        index_list.push(i);
+        index_list.push(i+1);
+        index_list.push(innerIndexStart+1);
+    }
+    //right
+    for(let i=0;i<Math.floor(tableHeight/2);i++) {
+        index_list.push(tableWidth+i);
+        index_list.push(tableWidth+i+1);
+        index_list.push(innerIndexStart+1);
+    }
+    index_list.push(tableWidth+Math.floor(tableHeight/2));
+    index_list.push(innerIndexStart+1);
+    index_list.push(innerIndexStart+2);
+    for(let i=Math.floor(tableHeight/2);i<tableHeight;i++) {
+        index_list.push(tableWidth+i);
+        index_list.push(tableWidth+i+1);
+        index_list.push(innerIndexStart+2);
+    }
+    //bottom
+    for(let i=0;i<Math.floor(tableWidth/2);i++) {
+        index_list.push(tableWidth+tableHeight+i);
+        index_list.push(tableWidth+tableHeight+i+1);
+        index_list.push(innerIndexStart+2);
+    }
+    index_list.push(tableWidth+tableHeight+Math.floor(tableWidth/2));
+    index_list.push(innerIndexStart+2);
+    index_list.push(innerIndexStart+3);
+    for(let i=Math.floor(tableWidth/2);i<tableWidth;i++) {
+        index_list.push(tableWidth+tableHeight+i);
+        index_list.push(tableWidth+tableHeight+i+1);
+        index_list.push(innerIndexStart+3);
+    }
+    //left
+    for(let i=0;i<Math.floor(tableHeight/2);i++) {
+        index_list.push(2*tableWidth+tableHeight+i);
+        index_list.push(2*tableWidth+tableHeight+i+1);
+        index_list.push(innerIndexStart+3);
+    }
+    index_list.push(2*tableWidth+tableHeight+Math.floor(tableHeight/2));
+    index_list.push(innerIndexStart+3);
+    index_list.push(innerIndexStart);
+    for(let i=Math.floor(tableHeight/2);i<tableHeight-1;i++) {
+        index_list.push(2*tableWidth+tableHeight+i);
+        index_list.push(2*tableWidth+tableHeight+i+1);
+        index_list.push(innerIndexStart);
+    }
+    index_list.push(2*tableWidth+2*tableHeight-1);
+    index_list.push(0);
+    index_list.push(innerIndexStart);
+    //center
+    index_list.push(innerIndexStart);
+    index_list.push(innerIndexStart+1);
+    index_list.push(innerIndexStart+2);
+    index_list.push(innerIndexStart);
+    index_list.push(innerIndexStart+2);
+    index_list.push(innerIndexStart+3);
+
     let h_num=0,w_num=0;
     let translateH=0,translateW=0;
     let translateH_uv = 0,translateW_uv = 0;
@@ -398,14 +460,12 @@ function updateGeometry(h,w) {
         translateW = w_num*stepWidth_pix;
         translateW_uv = w_num/distortWidth;
     }
-    //console.log(`h = ${h}, w = ${w}`);
-    //console.log(`h_num = ${h_num}, w_num = ${w_num}`);
-    for(let i=4*2;i<8*2;i+=2) {
+    //for(let i=4*2;i<8*2;i+=2) {
+    for(let i=innerIndexStart*2;i<(innerIndexStart+4)*2;i+=2) {
         pos_list[i+1] += translateH;
         pos_list[i] += translateW;
         pos_list[i+1] = Math.min(Math.max(0,pos_list[i+1]),distortHeight_pix);
         pos_list[i] = Math.min(Math.max(0,pos_list[i]),distortWidth_pix);
-        //console.log(`(w,h) = ${pos_list[i]},${pos_list[i+1]}`);
 
         pos_list_uv[i+1] += translateH_uv;
         pos_list_uv[i] += translateW_uv;
@@ -433,5 +493,30 @@ function updateGeometry(h,w) {
         .addIndex(index_list);
     return geometry;
 }
-
-
+function updateLinechartsSprite(h,w) {
+    let frameWidth = (distortWidth-focalWidth)/2;
+    let frameHeight = (distortHeight-focalHeight)/2;
+    w = Math.max(frameWidth,Math.min(w,tableWidth-frameWidth-1));
+    h = Math.max(frameHeight,Math.min(h,tableHeight-frameHeight-1));
+    const focalOrigin = [(distortWidth-focalWidth*fisheyeScale)/2*stepWidth_pix,(distortHeight-focalWidth*fisheyeScale)/2*stepHeight_pix];
+    
+    let startW = w-Math.floor(distortWidth/2);
+    let startH = h-Math.floor(distortHeight/2);
+    //console.log(`h = ${h}  w = ${w}`);
+    let bgTexture = createBackgroundTexture(startH,startW,startH+distortHeight-1,startW+distortWidth-1);
+    quad.shader.uniforms.uSampler2=bgTexture;
+    //quad.x = startW*stepWidth_pix;
+    //quad.y = startH*stepHeight_pix;
+    quad.x=Math.min(startW*stepWidth_pix,stageWidth_pix-focalOrigin[0]-focalWidth*fisheyeScale*stepWidth_pix);
+    quad.y=Math.min(startH*stepHeight_pix,stageHeight_pix-focalOrigin[1]-focalHeight*fisheyeScale*stepHeight_pix);
+    let innerW = w-Math.floor(focalWidth/2);
+    let innerH = h-Math.floor(focalHeight/2);
+    let linechartsTexture = createLineChartsTexture(stepHeight_pix*fisheyeScale,stepWidth_pix*fisheyeScale,innerH,innerW,innerH+focalHeight-1,innerW+focalWidth-1);
+    linechartsSprite.texture = linechartsTexture;
+    linechartsSprite.x = startW*stepWidth_pix+focalOrigin[0];
+    linechartsSprite.y = startH*stepHeight_pix+focalOrigin[1];
+    linechartsSprite.h=h;
+    linechartsSprite.w = w;
+}
+container.addChild(quad);
+//container.addChild(linechartsSprite);
