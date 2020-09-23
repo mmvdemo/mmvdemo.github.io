e5460c20a3399c250a444829ec87bc3cfae609df
Wendyx <xiawenyu9802@163.com>
2020-09-23 20:24:32 +0800

    add csvtojson url

--- app.js
+++ app.js
@@ -6,40 +6,40 @@ const fs = require("fs");
 const app = express();
 app.engine("html",ejs.renderFile);
 
-const DATA = "RANDOM";
-//load data
-let json = {};
-let timeStart,timeEnd,nodeCnt;
-if (DATA == "RANDOM") {
-    timeStart = 2012;
-    timeEnd = 2019;
-    nodeCnt = 51;
-} else if(DATA == "US") {
-    timeStart = 2005;
-    timeEnd = 2018;
-    for(let i=2005;i<=2018;i++) {
-        let dataFilePath = `static/US/${i}.csv`;
-        csvtojson().fromFile(dataFilePath).then((jsonObj)=>{json[`${i}`]=JSON.stringify(jsonObj)});
-    }
-}
+//const DATA = "RANDOM";
+////load data
+//let json = {};
+//let timeStart,timeEnd,nodeCnt;
+//if (DATA == "RANDOM") {
+//    timeStart = 2012;
+//    timeEnd = 2019;
+//    nodeCnt = 51;
+//} else if(DATA == "US") {
+//    timeStart = 2005;
+//    timeEnd = 2018;
+//    for(let i=2005;i<=2018;i++) {
+//        let dataFilePath = `static/US/${i}.csv`;
+//        csvtojson().fromFile(dataFilePath).then((jsonObj)=>{json[`${i}`]=JSON.stringify(jsonObj)});
+//    }
+//}
 
 app.use(express.static(__dirname));
 app.get('/',function(req,res){
     res.render(path.join(__dirname,'index.html'));
 })
-app.get('/cartesianlens',function(req,res){
-    res.render(path.join(__dirname,'index.html'), {technique:"cartesianlens",dataType:DATA,timeStart:timeStart,timeEnd:timeEnd,nodeCnt:nodeCnt,data:JSON.stringify(json)});
-})
-app.get('/tablelens',function(req,res){
-    res.render(path.join(__dirname,'index.html'), {technique:"tablelens",dataType:DATA,timeStart:timeStart,timeEnd:timeEnd,nodeCnt:nodeCnt,data:JSON.stringify(json)});
-})
-app.get('/fisheyelens',function(req,res){
-    res.render(path.join(__dirname,'index.html'), {technique:"fisheyelens",dataType:DATA,timeStart:timeStart,timeEnd:timeEnd,nodeCnt:nodeCnt,data:JSON.stringify(json)});
-})
-app.get('/zoom',function(req,res){
-    res.render(path.join(__dirname,'index.html'), {technique:"zoom",dataType:DATA,timeStart:timeStart,timeEnd:timeEnd,nodeCnt:nodeCnt,data:JSON.stringify(json)});
-})
-app.get('/inset',function(req,res){
-    res.render(path.join(__dirname,'index.html'), {technique:"inset",dataType:DATA,timeStart:timeStart,timeEnd:timeEnd,nodeCnt:nodeCnt,data:JSON.stringify(json)});
-})
+//app.get('/cartesianlens',function(req,res){
+//    res.render(path.join(__dirname,'index.html'), {technique:"cartesianlens",dataType:DATA,timeStart:timeStart,timeEnd:timeEnd,nodeCnt:nodeCnt,data:JSON.stringify(json)});
+//})
+//app.get('/tablelens',function(req,res){
+//    res.render(path.join(__dirname,'index.html'), {technique:"tablelens",dataType:DATA,timeStart:timeStart,timeEnd:timeEnd,nodeCnt:nodeCnt,data:JSON.stringify(json)});
+//})
+//app.get('/fisheyelens',function(req,res){
+//    res.render(path.join(__dirname,'index.html'), {technique:"fisheyelens",dataType:DATA,timeStart:timeStart,timeEnd:timeEnd,nodeCnt:nodeCnt,data:JSON.stringify(json)});
+//})
+//app.get('/zoom',function(req,res){
+//    res.render(path.join(__dirname,'index.html'), {technique:"zoom",dataType:DATA,timeStart:timeStart,timeEnd:timeEnd,nodeCnt:nodeCnt,data:JSON.stringify(json)});
+//})
+//app.get('/inset',function(req,res){
+//    res.render(path.join(__dirname,'index.html'), {technique:"inset",dataType:DATA,timeStart:timeStart,timeEnd:timeEnd,nodeCnt:nodeCnt,data:JSON.stringify(json)});
+//})
 app.listen(3000);
