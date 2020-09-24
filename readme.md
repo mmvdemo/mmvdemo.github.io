da713dfee0c2572bfff9f6e759796b279928c307
Wendyx <xiawenyu9802@163.com>
2020-09-24 10:30:46 +0800

    fix cartesian lens interactivity

--- readme.md
+++ readme.md
@@ -1,3 +1,17 @@
 # Matrix visualization
 
-+ change app.js to run zoom or lens
++ All source codes hosted in index.html
++ Source codes are under src/
++ Running on Github Pages: https://wenyux.github.io/matrix-visualization/
+
++ data type
+    + random data: need to specify node count.
+    + US migration data.
+
++ techniques
+    + lenses
+        + cartesian lens
+        + table lens
+        + fisheye lens
+    + zoom
+
--- src/cartesianLens.js
+++ src/cartesianLens.js
@@ -2,6 +2,7 @@ import * as PARA from "./parameters.js";
 import {createGridGeometry,createBackgroundTexture,createLineChartsTexture} from "./utils.js";
 let d=10;
 let focusPos = {'w':-1,'h':-1};
+let focusPos_pix = {'w':-1,'h':-1};
 let quad;
 const vertexSrc = `
 
@@ -40,36 +41,40 @@ function g1(Dmax,Dnorm) {
     }
     return g;
 }
+function getCartesianPosition(s,foc,x) {
+    let sign,Dmax;
+    if(x<foc) {
+        Dmax = foc;
+        sign = -1;
+    } else {
+        Dmax = PARA.table[s]-foc;
+        sign = 1;
+    }
+    let scale = g1(Dmax,Math.abs(x-foc));
+    return (foc+sign*Dmax*scale)*PARA.step_pix[s];
+}
+function binSearch(n,s,lo,hi) {
+    while(lo<hi) {
+        let mi = Math.floor((lo+hi)/2);
+        if(n<getCartesianPosition(s,focusPos[s],mi)) {
+            hi=mi;
+        } else {
+            lo = mi+1;
+        }
+    }
+    return lo-1;
+}
 function updateQuad(h,w) {
+    focusPos.h = h>PARA.table.h/2?h+1:h;
+    focusPos.w = w>PARA.table.w/2?w+1:w;
+    
     const buffer = quad.geometry.getBuffer('aVertexPosition');
     for(let i=0;i<=PARA.table.h;i++) {
         for(let j=0;j<=PARA.table.w;j++) {
-            let sign={},Dmax = {};
-            if(i<h) {
-                Dmax['h'] = h;
-                sign['h'] = -1;
-            } else {
-                Dmax['h'] = PARA.table.h-h;
-                sign['h'] = 1;
-            }
-            if(j<w) {
-                Dmax['w'] = w;
-                sign['w']=-1;
-            }else {
-                Dmax['w'] = (PARA.table.w-w);
-                sign['w']=1;
-            }
-            
-            let scale = {
-                'w':g1(Dmax.w,Math.abs(j-w)),
-                'h':g1(Dmax.h,Math.abs(i-h))};
-
-            buffer.data[2*bufferIndex(i,j)+1] = (h+sign.h*Dmax.h*scale.h)*PARA.step_pix.h;
-            buffer.data[2*bufferIndex(i,j)] = (w+sign.w*Dmax.w*scale.w)*PARA.step_pix.w;
+            buffer.data[2*bufferIndex(i,j)+1] = getCartesianPosition('h',focusPos.h,i);
+            buffer.data[2*bufferIndex(i,j)] = getCartesianPosition('w',focusPos.w,j);
         }
     }
-    focusPos.h = h;
-    focusPos.w = w;
     buffer.update();
 };
 let app;
@@ -97,8 +102,14 @@ export function loadCartesianLens() {
     canvas.addEventListener('mousemove',function(evt) {
         const rect = canvas.getBoundingClientRect();
         const mouseOnCanvas = {'h':evt.clientY-rect.top-container.y,'w':evt.clientX-rect.left-container.x};
-        let w = Math.floor(mouseOnCanvas.w/PARA.step_pix.w);
-        let h = Math.floor(mouseOnCanvas.h/PARA.step_pix.h);
+        let h,w;
+        if(focusPos.h<0 || focusPos.w<0) {
+            w = Math.floor(mouseOnCanvas.w/PARA.step_pix.w);
+            h = Math.floor(mouseOnCanvas.h/PARA.step_pix.h);
+        } else {
+            w = binSearch(mouseOnCanvas.w,'w',0,PARA.table.w);
+            h = binSearch(mouseOnCanvas.h,'h',0,PARA.table.h);
+        }
         console.log(`h=${h},w=${w}`);
         if(w<0||h<0||w>=PARA.table.w+1||h>=PARA.table.h+1) {
             return;
